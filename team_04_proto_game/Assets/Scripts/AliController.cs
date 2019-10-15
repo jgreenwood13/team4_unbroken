160926a98c743b0c737f86ed69bb626ce55415cc
jgreenwo13 <jhamieka.greenwood@gmail.com>
2019-10-15 02:17:34 -0400

    Added jumping animations to Ali activated by space bar, not as smooth but can jump

--- team_04_proto_game/Assets/Scripts/AliController.cs
+++ team_04_proto_game/Assets/Scripts/AliController.cs
@@ -21,12 +21,18 @@ public class AliController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        moveHorizontal();
+        Jump();
+    }
+
+    void moveHorizontal()
+    {
         //print("Update");
         //xposition = Input.GetAxis("Horizontal");  //left and right movement
         xposition = Input.GetAxis("Horizontal");
         print(xposition);
 
-         //if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        //if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         if (xposition > 0.5)
         {
             print("if statement");
@@ -49,6 +55,19 @@ public class AliController : MonoBehaviour
             animator.SetBool("IsRunning", false);
             transform.position += new Vector3(xposition, 0f, 0f) * moveSpeed * Time.deltaTime;
 
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("IsJumping", true);
+            animator.SetBool("IsLanding", true);
+        }
+        else
+        {
+            animator.SetBool("IsJumping", false);
         }
     }
 }
