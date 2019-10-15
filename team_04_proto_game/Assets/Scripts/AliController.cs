4cee942ff2d286c45a13cb550b440740c81f0d94
jgreenwo13 <jhamieka.greenwood@gmail.com>
2019-10-15 00:12:34 -0400

    Player falls off game when turning left and right, adjusted box collider to match direction of player movement. Fixed player controller script and added cinemachine camera for better player to camera motions

--- team_04_proto_game/Assets/Scripts/AliController.cs
+++ team_04_proto_game/Assets/Scripts/AliController.cs
@@ -7,27 +7,48 @@ public class AliController : MonoBehaviour
     private float moveSpeed;
     private Animator animator;
     private float xposition;
-    private bool facingRight = true;
     private Vector3 localRotation;
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+        moveSpeed = 3.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");  //left and right movement
-        float vertical = Input.GetAxis("Vertical");  //up and down movement
+        //print("Update");
+        //xposition = Input.GetAxis("Horizontal");  //left and right movement
+        xposition = Input.GetAxis("Horizontal");
+        print(xposition);
 
-        //Debug.Log(horizontal);  //writes values to console screen
-        Debug.Log(vertical); 
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+         //if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        if (xposition > 0.5)
+        {
+            print("if statement");
+            transform.localRotation = Quaternion.Euler(0f, 90f, 0f);
+            animator.SetBool("IsRunning", true);
+            transform.position += new Vector3(xposition, 0f, 0f) * moveSpeed * Time.deltaTime;
+        }
+        //else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        else if (xposition < -.5)
+        {
+            print("else if");
+            transform.localRotation = Quaternion.Euler(0f, -90f, 0f);
+            animator.SetBool("IsRunning", true);
+            transform.position += new Vector3(xposition, 0f, 0f) * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            print("else statement");
+            transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+            animator.SetBool("IsRunning", false);
+            transform.position += new Vector3(xposition, 0f, 0f) * moveSpeed * Time.deltaTime;
+
+        }
     }
 }
