72b1b591578eb1e6f612420496657840038a9987
hrc17b <hrc17b@my.fsu.edu>
2019-10-17 11:29:05 -0400

    Commit

--- team_04_proto_game/Assets/Scripts/AliController.cs
+++ team_04_proto_game/Assets/Scripts/AliController.cs
@@ -4,23 +4,72 @@ using UnityEngine;
 
 public class AliController : MonoBehaviour
 {
+    private float moveSpeed;
+    private Animator animator;
+    private float xposition;
+    private Vector3 localRotation;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+        moveSpeed = 3.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");  //left and right movement
-        float vertical = Input.GetAxis("Vertical");  //up and down movement
+        moveHorizontal();
+        Jump();
+    }
 
-        //Debug.Log(horizontal);  //writes values to console screen
-        Debug.Log(vertical); 
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+    void moveHorizontal()
+    {
+        //print("Update");
+        //xposition = Input.GetAxis("Horizontal");  //left and right movement
+        xposition = Input.GetAxis("Horizontal");
+        //print(xposition);
+
+        //if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        if (xposition > 0.5)
+        {
+            //print("if statement");
+            transform.localRotation = Quaternion.Euler(0f, 90f, 0f);
+            animator.SetBool("IsRunning", true);
+            transform.position += new Vector3(xposition, 0f, 0f) * moveSpeed * Time.deltaTime;
+        }
+        //else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        else if (xposition < -.5)
+        {
+            //print("else if");
+            transform.localRotation = Quaternion.Euler(0f, -90f, 0f);
+            animator.SetBool("IsRunning", true);
+            transform.position += new Vector3(xposition, 0f, 0f) * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            //print("else statement");
+            transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+            animator.SetBool("IsRunning", false);
+            //transform.position += new Vector3(xposition, 0f, 0f) * moveSpeed * Time.deltaTime;
+
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("jump on");
+            animator.SetBool("IsJumpingOn", true);
+            //animator.SetBool("IsJumpingOn", false);
+        }
+        else
+        {
+            print("jump off");
+            animator.SetBool("IsJumpingOn", false);
+        }
     }
 }
