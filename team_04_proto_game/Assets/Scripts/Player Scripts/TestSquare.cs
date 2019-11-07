a13bf5a167bd53473150d45197924019f5c98153
jgreenwo13 <jhamieka.greenwood@gmail.com>
2019-11-07 07:52:48 -0500

    Asset folder organization and created new scene

---
+++ team_04_proto_game/Assets/Scripts/Player Scripts/TestSquare.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSquare : MonoBehaviour
+{
+    public float Speed;
+
+    private void Update()
+    {
+        if (VirtualInputManager.Instance.MoveRight && VirtualInputManager.Instance.MoveLeft)
+        {
+            return;
+        }
+
+        if (VirtualInputManager.Instance.MoveRight)
+        {
+            this.gameObject.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+            this.gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+
+        if (VirtualInputManager.Instance.MoveLeft)
+        {
+            this.gameObject.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+            this.gameObject.transform.rotation = Quaternion.Euler(0f, -180f, 0f);
+        }
+    }
+}
