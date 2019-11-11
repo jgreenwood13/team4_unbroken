63636ccf950c0e9e6097829efd433f2e0548bc30
jgreenwo13 <jhamieka.greenwood@gmail.com>
2019-11-11 04:20:20 -0500

    Imported bomb pack and unity particle effects pack. Added bomb asset, timed explosion with script and created a fire particle after effect. Added desctructible script to destroy objects (need compatible colliders on destruction object and surrounding objects)

---
+++ team_04_proto_game/Assets/Scripts/Explosion/Destructible.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    public GameObject destroyedVersions;
+
+    public void Destroy()
+    {
+        Instantiate(destroyedVersions, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+}
